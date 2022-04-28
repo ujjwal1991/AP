89feed7a16d918d039e3d6cbc8d870dbbd61bb37
ujjwal kumar <ujjwal.gp@gmail.com>
2022-04-28 22:55:27 +0530

    added cart page

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+      
+	public CartPage() {
+		System.out.println("Cart page");
+	}
+	
+	public void addToCart() {
+		System.out.println("add to cart");
+	}
+}
